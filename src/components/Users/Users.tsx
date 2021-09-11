f96b389b9b7ac6d890cc2afba4ec8264ca1e581f
TonyBNK <borisenk-anton@yandex.ru>
2021-09-11 13:23:13 +0300

    54. Used componentDidMount() instead of constructor.

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -9,47 +9,47 @@ type UsersPropsType = {
     followUnfollow: (id: number) => void
     setUsers: (users: Array<UserType>) => void
 }
-export const Users: React.FC<UsersPropsType> = (
-    {
-        users,
-        followUnfollow,
-        setUsers
-    }
-) => {
-    if (users.length === 0) {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users`).then(response => {
-            setUsers(response.data.items);
-        });
 
+export class Users extends React.Component<UsersPropsType> {
+    componentDidMount() {
+        if (this.props.users.length === 0) {
+            axios.get(`https://social-network.samuraijs.com/api/1.0/users`).then(response => {
+                this.props.setUsers(response.data.items);
+            });
+
+        }
     }
 
-    const usersList = users.map(u => {
-            const onClickHandler = () => {
-                followUnfollow(u.id);
-            }
+    render = () => {
+        const usersList = this.props.users.map(u => {
+                const onClickHandler = () => {
+                    this.props.followUnfollow(u.id);
+                }
 
-            return <div>
-                <div className={c.user} key={u.id}>
-                    <img src={u.photos.small ? u.photos.small : catUser} alt="ava"/>
-                    <button onClick={onClickHandler}>
-                        {u.followed ? 'Unfollow' : 'Follow'}
-                    </button>
-                    <div className={c.body}>
-                        <span className={c.name}>{u.name}</span>
-                        <span className={c.text}>{u.status}</span>
-                        <span
-                            className={c.address}>{'u.address.country'}, {'u.address.city'}</span>
+                return <div>
+                    <div className={c.user} key={u.id}>
+                        <img src={u.photos.small ? u.photos.small : catUser}
+                             alt="ava"/>
+                        <button onClick={onClickHandler}>
+                            {u.followed ? 'Unfollow' : 'Follow'}
+                        </button>
+                        <div className={c.body}>
+                            <span className={c.name}>{u.name}</span>
+                            <span className={c.text}>{u.status}</span>
+                            <span
+                                className={c.address}>{'u.address.country'}, {'u.address.city'}</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-        }
-    );
+            }
+        );
 
-    return (
-        <div className={c.users}>
-            <h3>Users</h3>
-            <span>{usersList}</span>
-            <button className={c.show}>Show more</button>
-        </div>
-    )
+        return (
+            <div className={c.users}>
+                <h3>Users</h3>
+                <span>{usersList}</span>
+                <button className={c.show}>Show more</button>
+            </div>
+        )
+    }
 }
